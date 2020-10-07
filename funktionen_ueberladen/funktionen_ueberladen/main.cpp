491a0d0a7efc79916848c758e7d40edb0603bd91
Mark Baumann <57982395+marky-mark-88@users.noreply.github.com>
2020-10-07 19:36:38 +0200

    funktionen überladen

--- funktionen_ueberladen/funktionen_ueberladen/main.cpp
+++ funktionen_ueberladen/funktionen_ueberladen/main.cpp
@@ -7,9 +7,47 @@
 //
 
 #include <iostream>
-
+#include <string>
+ 
+using namespace std;
+int maximum(int a, int b) {
+    cout << "maximum(a, b)" << endl;
+    if (a >= b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+int maximum(int a, int b, int c) {
+    cout << "maximum(a, b, c)" << endl;
+    if (a >= b && a >= c) {
+        return a;
+    }
+    if (b >= a && b >= c) {
+        return b;
+    }
+    if (c >= a && c >= b) {
+        return c;
+    }
+    return a;
+}
+int maximum(int a, int b, int c, int d) {
+    cout << "maximum(a, b, c, d)" << endl;
+    if (a >= b && a >= c && a >= d) {
+        return a;
+    }
+    if (b >= a && b >= c && b >= d) {
+        return b;
+    }
+    if (c >= a && c >= b && c >= d) {
+        return c;
+    }
+    if (d >= a && d >= b && d >= c) {
+        return d;
+    }
+    return a;
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    cout << maximum(1, 2, 3) << endl;
 }

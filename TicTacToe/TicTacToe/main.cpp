9d134c91bccfd2a6e6fde8b49b3658949f75aaa8
Mark Baumann <57982395+marky-mark-88@users.noreply.github.com>
2020-10-16 14:28:45 +0200

    FizzBuzz and TicTacToe start

---
+++ TicTacToe/TicTacToe/main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by Mark Baumann   on 15.10.20.
+//  Copyright © 2020 Mark Baumann. All rights reserved.
+//
+
+#include <iostream>
+#include <array>
+
+
+using namespace std;
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    //Array erzeugen
+    array<array<int, 3>, 3> field;
+    
+    //Daten einfügen
+    field[0] = {1, 0, 0};
+    field[1] = {1, 0, 0};
+    field[2] = {1, 0, 0};
+    
+    //ausgeben wer gewonnen hat
+    for (int i = 0; i <= 2; i++) {
+        if(field[i][0] != 0 && field[i][0] == field[i][1] &&  field[i][0] == field[i][2]) {
+            cout<< field[i][0] << " hat gewonnen \n";
+        }
+            
+    }
+    
+    if(field[0][i] !=0 && field)
+    
+    //Aufmalen des Array Spielfelds
+    cout << field[0][0] << " | " << field[0][1] << " | " << field[0][2] <<endl;
+    cout << field[1][0] << " | " << field[1][1] << " | " << field[1][2] <<endl;
+    cout << field[2][0] << " | " << field[2][1] << " | " << field[2][2] <<endl;
+}
---
+++ fizzbuzz/fizzbuzz/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  fizzbuzz
+//
+//  Created by Mark Baumann   on 12.10.20.
+//  Copyright © 2020 Mark Baumann. All rights reserved.
+//
+
+#include <iostream>
+#include <iostream>
+
+using namespace std;
+
+
+void fizzbuzz(int n) {
+    
+    
+    
+}
+
+
+int main(int argc, const char * argv[]) {
+    
+    cout << "fizbuzz(10):" << endl;
+       fizzbuzz(10);
+       cout << "-------------" << endl;
+       cout << "fizbuzz(20):" << endl;
+       fizzbuzz(20);
+    
+    
+    return 0;
+}
